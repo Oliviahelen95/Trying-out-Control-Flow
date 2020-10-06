71e7a85fe1bc46d031735813481c05d19abf6e29
Oliviahelen95 <72461988+Oliviahelen95@users.noreply.github.com>
2020-10-06 13:17:40 -0400

    Add files via upload

---
+++ Practice 1.py
@@ -0,0 +1,17 @@
+Python 3.9.0 (v3.9.0:9cf6752276, Oct  5 2020, 11:29:23) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> print 'Hello World'
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print('Hello World')?
+>>> print ('Hello World')
+Hello World
+>>> 
+>>> 
+>>> 
+>>> 
+>>> print ('Hello and welcome to THE GAME')
+Hello and welcome to THE GAME
+>>> print ('Please click on the following choices to decide your fate...choose carefully')
+Please click on the following choices to decide your fate...choose carefully
+>>> 
